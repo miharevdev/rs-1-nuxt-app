b1edd9a638e8e6752aa9fb65c98ad51746e61988
miharevdev <miharev.dev@gmail.com>
2024-03-03 17:16:19 +0300

    add: init

---
+++ components/Tutorial.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="showMessageFromBackend">Show message from backend</button>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+  methods: {
+    async showMessageFromBackend () {
+      try {
+        const response = await this.$axios.get('/api/test')
+        console.log(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
