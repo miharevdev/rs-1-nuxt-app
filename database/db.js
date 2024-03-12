da321869e83ef1e4fe95ab6875d31bb777df8025
miharevdev <miharev.dev@gmail.com>
2024-03-12 23:07:31 +0300

    fix

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "samaellilith",
-    host: "127.0.0.1",
+    host: "localhost",
     port: 5432,
-    database: "test_db"
+    database: "test_vl"
 });
 
 module.exports = pool;
