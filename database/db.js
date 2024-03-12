094ba91724cb4342bcefe41843192fc9a3d564f8
miharevdev <miharev.dev@gmail.com>
2024-03-12 22:50:15 +0300

    test: db connect

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "samaellilith",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 5432,
     database: "test_vl"
 });
